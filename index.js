830967e326f8fd20c1580e70af85b3b93939f5ea
Seonghyunsun <leaf1993@naver.com>
2020-10-28 23:55:23 +0900

    EJS로 Dynamic Website 만들기

--- index.js
+++ index.js
@@ -6,14 +6,29 @@ const app = express();
 // app.get('/', function(req, res) { // '/'위치에 get요청 받은 경우
 //   res.send('Hello World!'); // Hello World를 보낸다.
 // });
-
+app.set('view engine', 'ejs');
+// ejs를 사용하기 위해서 express의 view engine에 ejs를 set하는 코드
 app.use(express.static(__dirname + '/public'));
 // app.use(콜백_함수)형태
 // app.get과 다르게 HTTP method나 route에 상관없이 서버 요청올때마다 무조건 콜백함수로 자동 저장
-// __dirname은 실행중인 파일의 위치를 나타내는 global variable.
+// __dirname은 실행중인 파일의 위치 나타내는 global variable.
 // 현재_위치/publlic   route를 static폴더로 지정하라는 명령
 //  '/'에 접속하면 '현재_위치/public'에 접속되고
 //  '/css'에 접속하면 '현재_위치/public/css'에 접속됨
+
+app.get('/hello', function(req,res){
+  res.render('hello', {name:req.query.nameQuery});
+});
+// query를 통해서 이름을 받음. query들은 req.query에 저장됨
+
+app.get('/hello/:nameParam', function(req,res){
+  res.render('hello', {name:req.params.nameParam});
+});
+// route parameter를 통해 이름을 받는 코드
+// 콜론(:)으로 시작되는 route는 해당 부분에 입력되는 route의 텍스트가 req.params에 저장된다.
+// ejs파일을 사용하기 위해 res.render함수를 사용한다.
+// 첫번째 parameter로 ejs의 이름을 , 두번째 parameter로 ejs에서 사용될 object전달
+// res.render함수는 ejs를 /views폴더에서 찾으므로 views폴더 이름 변경 x
 
 var port = 3000;
 // 사용할 포트 번호를 port변수에 넣는다.
